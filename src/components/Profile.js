32160db4122ac66c9daefa593bbd4612b8781075
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-06-18 16:11:19 +0800

    logout & edit profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
 
 // MUI Stuff
 import Button from '@material-ui/core/Button';
@@ -21,6 +22,7 @@ import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
 import CalendarToday from '@material-ui/icons/CalendarToday';
 import EditIcon from '@material-ui/icons/Edit';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 
 const styles = (theme) => ({
     paper: {
@@ -84,6 +86,10 @@ class Profile extends Component {
         fileInput.click();
     };
 
+    handleLogout = () => {
+        this.props.logoutUser();
+    }
+
     render() {
         const { classes, 
             user: { credentials: { handle, createdAt, imageUrl, bio, website, location },
@@ -134,6 +140,12 @@ class Profile extends Component {
                         <CalendarToday color="primary"/>{' '}
                         <span> joined {dayjs(createdAt).format('MMM YYYY')} </span>
                     </div>
+                    <Tooltip title="Logout" placement="top">
+                        <IconButton onClick={this.handleLogout}>
+                            <KeyboardReturn color="primary"/>
+                        </IconButton>
+                    </Tooltip>
+                    <EditDetails/>
                 </div>
             </Paper>) 
             : (<Paper className={classes.paper}>
