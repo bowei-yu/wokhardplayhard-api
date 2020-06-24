7a6581675364b0dad11382f9517cbcf0aea8fbd3
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-06-24 16:48:57 +0800

    minor changes for redux devtool extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,9 @@ const reducers = combineReducers({
 
 const store = createStore(reducers, initialState, 
     compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ 
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
     )
 );
 
